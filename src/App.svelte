5c4e68f9d6f1d383f6883688abf2c73c836a1d62
adityaoberai <adityaoberai1@gmail.com>
2023-12-01 20:04:38 +0530

    Update styles

--- src/App.svelte
+++ src/App.svelte
@@ -2,13 +2,16 @@
   import AltText from './components/AltText.svelte'
 </script>
 
-<section id="sponsor">
-  <a class="sponsor-image" href="https://getautoseo.com/image-alt-text-generator" target="_blank">
-    <img src="/sponsor.png" alt="AutoSEO Image Alt Text Generator">
-  </a>
-</section>
-
 <main>
+  <div class="sponsor align-main">
+    <label class="sponsor-label" for="sponsor-link">
+      Thank you to our sponsor!
+      <a id="sponsor-link" class="sponsor-link" href="https://getautoseo.com/image-alt-text-generator" target="_blank">
+        <img class="sponsor-image" src="/sponsor.png" alt="AutoSEO Alt Text Generator">
+      </a>
+    </label>
+  </div>
+
   <div class="align-main">
     <h1>Alt Text Generator</h1>
 
@@ -16,20 +19,26 @@
   
     <AltText />
   </div>
-</main>
-<div class="footer">
-  <p>Support this project by <a href="https://github.com/adityaoberai/Alt-Text-Generator" target="_blank">starring ⭐</a> and <a href="https://github.com/sponsors/adityaoberai" target="_blank">sponsoring 💸</a> on GitHub</p>
-</div>
 
+  <div class="footer">
+    <p>Support this project by <a href="https://github.com/adityaoberai/Alt-Text-Generator" target="_blank">starring ⭐</a> and <a href="https://github.com/sponsors/adityaoberai" target="_blank">sponsoring 💸</a> on GitHub</p>
+  </div>
+</main>
 
 <style>
   h1 {
-    font-size: 3.2em;
+    font-size: 4rem;
     line-height: 1.1;
     max-width: 80vw;
   }
 
   p {
+    font-size: 1.5rem;
     max-width: 80vw;
+  }
+
+  .footer p {
+    max-width: 80vw;
+    font-size: 1rem;
   }
 </style>
